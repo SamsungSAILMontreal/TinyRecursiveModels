a66deaceeb83bba5a83e4b121fb0e67965b62203
Guilherme Cano Lopes <guilhermecanolopes@192.168.0.5>
2025-12-16 20:58:56 -0300

    feat: adjust pyproject and add download dataset to instal.sh

--- install.sh
+++ install.sh
@@ -20,6 +20,9 @@ echo "uv version: $(uv --version)"
 echo "Installing dependencies..."
 uv sync
 
+echo "Downlading datasets..."
+uv run python dataset/build_sudoku_dataset.py --output-dir data/sudoku-extreme-1k-aug-1000  --subsample-size 1000 --num-aug 1000
+
 echo ""
 echo "=== Setup complete! ==="
 echo ""
